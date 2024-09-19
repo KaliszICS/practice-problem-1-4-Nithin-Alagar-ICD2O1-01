9cb33b6eb6909a52a8f7ced014229f33c2f7231a
Nithin-Alagar-ICD2O1-01 <nithin.alagar@student.tdsb.on.ca>
2024-09-19 14:02:33 +0000

    nithin

--- PP1_4.py
+++ PP1_4.py
@@ -2,16 +2,22 @@
 
 def q1():
   #Write Assignment code here
-
+  word = input("Input a word: ")
+  print(word)
 def q2():
   #Write Assignment code here
-
+  name = input("Input your first name: ")
+  print("Hello "+name)
 def q3():
   #Write Assignment code here
-
+  firstname = input("Input your first name: ")
+  lastname = input("Input your last name: ")
+  print(f"{lastname} {firstname}")
 def q4():
   #Write Assignment code here
-
+  student1 = input("Input a student: ")
+  student2 = input("Input another student: ")
+  print(f"Your students are {student1} and {student2}")
 #Do not edit code below this comment
 
 q1()
