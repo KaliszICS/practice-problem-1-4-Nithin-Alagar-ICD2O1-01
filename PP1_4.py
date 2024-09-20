bc27415ba589bc0422e115a38cf5a93f44a22b3f
Nithin-Alagar-ICD2O1-01 <nithin.alagar@student.tdsb.on.ca>
2024-09-20 13:28:40 +0000

    imgay

--- PP1_4.py
+++ PP1_4.py
@@ -19,9 +19,8 @@ def q4():
   student2 = input("Input another student: ")
   print(f"Your students are {student1} and {student2}")
 #Do not edit code below this comment
-'''
-q1()
-q2()
-q3()
-q4()
-'''
+
+#q1()
+#q2()
+#q3()
+#q4()
